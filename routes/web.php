7646e0f1620e0642fa3e4579a8d342cf2ecd7184
defaltastra <defaltastra@gmail.com>
2023-02-16 19:36:40 +0100

    adding middleware

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ use App\Http\Controllers\UserController;
 */
 
 // User related routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 // Blog post related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustbeloggedin');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustbeloggedin');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
 
 
